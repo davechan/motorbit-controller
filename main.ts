16252392f7a1add9f3bff936fa9652fbcc4fd696
Dave Chan <davechan@users.noreply.github.com>
2021-03-26 20:03:06 +0800

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,38 @@
+input.onButtonPressed(Button.A, function () {
+    if (power <= 100) {
+        power += 20
+        radio.sendNumber(power)
+    }
+})
+// south
+input.onGesture(Gesture.LogoUp, function () {
+    if (power >= 0) {
+        power += -20
+        radio.sendNumber(power)
+    }
+})
+input.onButtonPressed(Button.B, function () {
+    if (power >= 0) {
+        power += -20
+        radio.sendNumber(power)
+    }
+})
+input.onGesture(Gesture.Shake, function () {
+    power = 0
+    radio.sendNumber(power)
+})
+// north
+input.onGesture(Gesture.LogoDown, function () {
+    if (power <= 100) {
+        power += 20
+        radio.sendNumber(power)
+    }
+})
+let power = 0
+radio.setGroup(1)
+radio.setTransmitPower(7)
+radio.sendNumber(0)
+power = 0
+basic.forever(function () {
+	
+})
