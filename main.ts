151089df99e119a50a952631255cd721ae05021d
Dave Chan <davechan@users.noreply.github.com>
2021-03-27 00:54:45 +0800

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,60 +1,60 @@
 input.onButtonPressed(Button.A, function () {
-    if (motor < 100) {
-        motor += 20
-        radio.sendValue("name", motor)
+    if (speed < 100) {
+        speed += 20
+        radio.sendValue("motor", speed)
     }
 })
 // south
 input.onGesture(Gesture.LogoUp, function () {
-    if (motor > 0) {
-        motor += -20
-        radio.sendValue("name", motor)
+    if (speed > 0) {
+        speed += -20
+        radio.sendValue("motor", speed)
     }
 })
 input.onGesture(Gesture.TiltLeft, function () {
-    servo += 30
-    radio.sendValue("name", servo)
+    angle += 30
+    radio.sendValue("servo", angle)
 })
 input.onButtonPressed(Button.AB, function () {
-    if (motor > 0) {
-        motor = 100
-        radio.sendValue("name", motor)
+    if (speed > 0) {
+        speed = 100
+        radio.sendValue("motor", speed)
     }
 })
 input.onButtonPressed(Button.B, function () {
-    if (motor > 0) {
-        motor += -20
-        radio.sendValue("name", motor)
+    if (speed > 0) {
+        speed += -20
+        radio.sendValue("motor", speed)
     }
 })
 input.onGesture(Gesture.Shake, function () {
-    motor = 0
-    radio.sendValue("name", motor)
+    speed = 0
+    radio.sendValue("motor", speed)
 })
 input.onGesture(Gesture.TiltRight, function () {
-    servo += -30
-    radio.sendValue("name", servo)
+    angle += -30
+    radio.sendValue("servo", angle)
 })
 // north
 input.onGesture(Gesture.LogoDown, function () {
-    if (motor < 100) {
-        motor += 20
-        radio.sendValue("name", motor)
+    if (speed < 100) {
+        speed += 20
+        radio.sendValue("motor", speed)
     }
 })
 input.onLogoEvent(TouchButtonEvent.Touched, function () {
-    servo = 0
-    radio.sendValue("name", servo)
+    angle = 0
+    radio.sendValue("servo", angle)
 })
-let motor = 0
-let servo = 0
+let speed = 0
+let angle = 0
 radio.setGroup(1)
 radio.setTransmitPower(7)
-servo = 0
-motor = 0
+angle = 0
+speed = 0
 basic.forever(function () {
     led.plotBarGraph(
-    motor,
+    speed,
     100
     )
 })
